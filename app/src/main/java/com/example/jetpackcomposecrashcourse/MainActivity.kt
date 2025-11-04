eacfe4f0db709c8f03f85a0935c77f9333302e55
nguonn <sengn5112@gmail.com>
2025-11-04 13:35:07 +0700

    lesson-creating-image-card-composable-done

--- app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,14 +22,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecrashcourse.ui.theme.JetPackComposeCrashCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,26 +46,74 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val painter = painterResource(id = R.drawable.eren)
+            val contentDescription = "Eren Yeager"
+            val title = "Eren Yeager"
 
-            Column(
-                modifier = Modifier
-                    .systemBarsPadding()
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f)
-                    .background(Color.Green)
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Yellow)
+            Box(
+                modifier = Modifier.fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ){
+                ImageCard(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    title = title
+                )
 
-            ) {
-                Text("Hello",
-                    modifier = Modifier.clickable{})
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("World")
             }
+
+
 
         }
     }
 }
+
+
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+        modifier: Modifier = Modifier
+    )
+    {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            elevation = CardDefaults.elevatedCardElevation(
+                defaultElevation = 5.dp
+            )
+
+        ){
+            Box (
+                modifier = Modifier.height(200.dp)
+            ){
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription ,
+                    contentScale = ContentScale.Crop
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ),
+                                startY = 300f
+                            )
+                        )
+                )
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+            }
+
+        }
+    }
+

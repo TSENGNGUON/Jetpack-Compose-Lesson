c99bdbd8cc3c066916c7bc8a416b4eef7a2097ac
nguonn <sengn5112@gmail.com>
2025-11-04 16:13:28 +0700

    lesson-textfield-buttons-showing-snackbar

--- app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
@@ -23,13 +23,18 @@ import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -51,6 +56,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecrashcourse.ui.theme.JetPackComposeCrashCourseTheme
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
     class MainActivity : ComponentActivity() {
@@ -58,48 +64,52 @@ import kotlin.random.Random
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-        Column (Modifier.fillMaxSize()){
-            val color = remember { mutableStateOf(Color.Yellow) }
-            ColorBox(
-                modifier =
-                    Modifier.weight(1f)
-                        .fillMaxSize()
+            val snackbarHostState = remember { SnackbarHostState() }
+            var textFieldState by remember { mutableStateOf("") }
+            val scope = rememberCoroutineScope()
+           Scaffold(
+               modifier = Modifier
+                   .fillMaxSize(),
+               snackbarHost = { SnackbarHost(snackbarHostState) }
+           ) {
+               Column(
+                   modifier = Modifier
+                       .padding(it).padding(horizontal = 30.dp)
+                       .fillMaxSize(),
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   verticalArrangement = Arrangement.Center,
+               ) {
 
-            ){
-                color.value = it
-            }
-            Box(modifier = Modifier
-                .background(color.value)
-                .weight(1f)
-                .fillMaxSize()
-            )
-        }
+                   TextField(
+                       value = textFieldState,
+                       onValueChange = {
+                           textFieldState = it
+                       },
+                       label = {
+                           Text("Enter Your Name ")
+                       },
+                       singleLine = true,
+                       modifier = Modifier.fillMaxWidth()
+
+                   )
+                   Spacer(modifier = Modifier.height(16.dp))
+                   Button(
+                       onClick = {
+                           scope.launch {
+                               snackbarHostState.showSnackbar("Hello $textFieldState")
+                           }
+                       }
+                   ) {
+                       Text("Pls greet me")
+                   }
+               }
+
+           }
+
         }
     }
 }
 
-    @Composable
-    fun ColorBox(
-        modifier: Modifier = Modifier,
-        updateColor: (Color) -> Unit
-    ){
-
-        Box(
-            modifier = modifier
-                .background(Color.Yellow)
-                .clickable{
-                    updateColor(
-                        Color(
-                            Random.nextFloat(),
-                            Random.nextFloat(),
-                            Random.nextFloat(),
-                            1f
-                        )
-                    )
-                }
-
-        )
-    }
 
 
 

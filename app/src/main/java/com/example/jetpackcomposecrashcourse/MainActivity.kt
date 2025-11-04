9537eff4e7ab47758954dcb5c2876d1e63e13445
nguonn <sengn5112@gmail.com>
2025-11-04 15:01:48 +0700

    lesson-styling-text-jetpack-compose-done

--- app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
@@ -35,7 +35,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,75 +54,59 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
+        )
         setContent {
-            val painter = painterResource(id = R.drawable.eren)
-            val contentDescription = "Eren Yeager"
-            val title = "Eren Yeager"
+           Box(
+               modifier = Modifier
+                   .systemBarsPadding()
+                   .fillMaxSize()
+                   .background(Color(0xFF101010))
+           ){
+               Text(
+                   fontSize = 30.sp,
+                   text = buildAnnotatedString {
+                       withStyle(
+                           style = SpanStyle(
+                               color = Color.Green,
+                               fontSize = 50.sp
 
-            Box(
-                modifier = Modifier.fillMaxWidth(0.5f)
-                    .padding(16.dp)
-            ){
-                ImageCard(
-                    painter = painter,
-                    contentDescription = contentDescription,
-                    title = title
-                )
+                           )
+                       ){
+                           append("J")
+                       }
+                       append("etpack ")
+                       withStyle(
+                           style = SpanStyle(
+                               color = Color.Green,
+                               fontSize = 50.sp
 
-            }
+                           )
+                       ){
+                           append("C")
+                       }
+                       append("ompose")
+                   },
+                   color = Color.White,
+                   fontFamily = fontFamily,
+                   fontWeight = FontWeight.Bold,
+                   fontStyle = FontStyle.Italic,
+                   textAlign = TextAlign.Center,
+                   textDecoration = TextDecoration.Underline
 
+               )
+           }
 
 
         }
     }
 }
 
-
-    @Composable
-    fun ImageCard(
-        painter: Painter,
-        contentDescription: String,
-        title: String,
-        modifier: Modifier = Modifier
-    )
-    {
-        Card(
-            modifier = modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(15.dp),
-            elevation = CardDefaults.elevatedCardElevation(
-                defaultElevation = 5.dp
-            )
-
-        ){
-            Box (
-                modifier = Modifier.height(200.dp)
-            ){
-                Image(
-                    painter = painter,
-                    contentDescription = contentDescription ,
-                    contentScale = ContentScale.Crop
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(
-                            Brush.verticalGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.Black
-                                ),
-                                startY = 300f
-                            )
-                        )
-                )
-                Box(
-                    modifier = Modifier.fillMaxSize().padding(12.dp),
-                    contentAlignment = Alignment.BottomStart
-                ) {
-                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-                }
-            }
-
-        }
-    }
 
